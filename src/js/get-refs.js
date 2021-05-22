848551d6edbb52bf00161e8b28db014bc6fdddf4
Maryna Berezhanska <arina.lex2012@gmail.com>
2021-05-22 12:13:15 +0300

    new built

--- src/js/get-refs.js
+++
@@ -1,7 +0,0 @@
-export default function getRefs(){
-    return {        
-        photosContainer: document.querySelector('.js-photos-container'),
-        searchForm: document.querySelector('.js-search-form'),
-        loadMoreBtn: document.querySelector('[data-action="load-more"]'),
-    };
-};
